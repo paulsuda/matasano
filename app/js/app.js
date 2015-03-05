56e4bb9cd0abe5034defb6657797ac8b68fccb81
Paul Suda <paul@manufaxure.com>
2015-03-05 12:24:11 -0600

    clean up some more and things still work

--- app/js/app.js
+++ app/js/app.js
@@ -8,12 +8,31 @@ var app = (function(document, $) {
 		_init = function() {
 			$(document).foundation();
 			_userAgentInit();
+		},
+		_switch_challenge = function(challenge_id) {
+			$('.challenge').hide();
+			var challenge_elem = $('#' + challenge_id);
+			if(challenge_elem.length){
+				challenge_elem.show();
+			}
+			else{
+				alert('No challenge found for: ' + challenge_id);
+			}
 		};
 	return {
-		init: _init
+		init: _init,
+		switch_challenge: _switch_challenge
 	};
 })(document, jQuery);
 
 (function() {
 	app.init();
 })();
+
+
+window.onpopstate = function (event) {
+	var hash = window.location.hash;
+	hash = hash.substring(1);
+	if(!hash) hash = 'set1challenge1';
+	app.switch_challenge(hash);
+};
