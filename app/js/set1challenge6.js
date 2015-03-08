9c2bbc8ab5055ec1590ffacf534ca01e48ff73ec
Paul Suda <paul@manufaxure.com>
2015-03-07 23:03:18 -0600

    setup to work on challenge 6

---
+++ app/js/set1challenge6.js
@@ -0,0 +1,31 @@
+'use strict';
+
+app.register_challenge((function(){
+
+  function Set1Challenge6(){
+    ChallengeBase.apply(this,['set1challenge6']);
+    var defaultStartKey = 'ICE';
+    var thisObj = this;
+    this.setInputDefaults([
+      'loading...',
+      defaultStartKey
+    ]);
+    $.get('/example_data/set1challenge6.txt', function(input_data){
+      /* Set defaults from file input. */
+      thisObj.setInputDefaults([
+        input_data,
+        defaultStartKey
+      ]);
+    });
+    return this;
+  }
+
+  Set1Challenge6.prototype = Object.create(ChallengeBase.prototype);
+
+  Set1Challenge6.prototype.compute = function(input_values){
+    var encoder_output = input_values[0];
+    return encoder_output;
+  };
+
+  return new Set1Challenge6();
+})());
